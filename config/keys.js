d0961500000fd1397f21a749928c5f3ae3ccba31
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2019-11-17 20:00:08 +0100

    Passport JWT Authentication Strategy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://riadhaylala:riadhaylala@haylalacluster-l8zqs.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://riadhaylala:riadhaylala@haylalacluster-l8zqs.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'qwerty vs azerty'
 }
