3be487b52486d04526d2ea3d0032dd06e381ead3
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-01-17 17:17:06 +0100

    Create & Get Posts

---
+++ controllers/api/postController.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Post = require('../../models/Post');
+const validatePostInput = require('../../validation/post');
+
+
+
+exports.getPosts = (req, res) => {
+    Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+};
+
+exports.getPostById = (req, res) => {
+    Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
+    );
+};
+
+exports.createPost = (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+};
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const postController = require('../../controllers/api/postController');
 
 // @route  GET api/posts/test
 // @desc   Test posts route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "posts router works"}));
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', postController.getPosts);
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', postController.getPostById);
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  postController.createPost
+);
 
 module.exports = router;
